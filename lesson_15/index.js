ce3d1c483c3ac31f064c98735544bd51924c0094
dt-lyudmi <lyudmidats@gmail.com>
2018-05-22 21:30:54 +0300

    fixed 2 lesson_15

--- lesson_15/index.js
+++ lesson_15/index.js
@@ -1,39 +1,53 @@
-function init(arr1, n){
-		var attrib = arr1.attributes;
-		var keyAtribute  = "";
-		var nameAtribute = "";
-		for (let key in attrib) {
-
-			keyAtribute = key;
-			nameAtribute = attrib[key];
+function init(arr, n) {
+	const ul = document.createElement('ul');
   
-		}
- 	var ul = document.createElement("ul");
-	var li ="";
- 	document.body.insertBefore(ul, document.body.firstChild);
-	for (var i = 0; i < n ; i++) {
-	
-		li = document.createElement("li");
-	    li.className = arr1.className;
-	    li.setAttribute(keyAtribute, nameAtribute);
-	    li.innerHTML = arr1.content;
-	    ul.appendChild(li);
+  for (let i = 0; i < arr.length; i += 1) {
+  	const el = arr[i];
+    
+    for (let j = 0; j < n; j += 1) {
+    		const li = document.createElement('li');
+        li.className = el.className;
+        
+/*         for (const key in el.attributes) {
+          li.setAttribute(key, el.attributes[key]);
+        } */
+        
+        const keys = Object.keys(el.attributes);
+        
+        for (let l = 0; l < keys.length; l += 1) {
+        	const key = keys[l];
 
-	}
-
+          li.setAttribute(key, el.attributes[key]);
+        }
+        
+        li.appendChild(document.createTextNode(el.content));
+        
+        ul.appendChild(li);
+    }
+  }
+  
+  document.body.appendChild(ul);
 }
 
- function listen() {
- 	document.querySelector('button').addEventListener("click", function(){
- 		var element = document.querySelector('ul');
-			if(element){
-				element.parentNode.removeChild(element);
-				
-				return true;
-			}
-			else{
-				
-				return false;
-			}
-	});	
+/* init([
+  { className: 'a1', attributes: { 'data-name': 'MyName', 'data-age': '43' }, content: '123' },
+  { className: 'a2', attributes: {}, content: '122456' }
+], 2);  */
+
+function listen() {
+	const b = document.getElementsByTagName('button')[0];
+  
+  b.addEventListener('click', function () {
+  	const ul = document.querySelector('ul');
+    
+    if (ul) {
+    	document.body.removeChild(ul);
+      
+      return true;
+    }
+    
+    return false;
+  });
 }
+
+/* listen(); */
